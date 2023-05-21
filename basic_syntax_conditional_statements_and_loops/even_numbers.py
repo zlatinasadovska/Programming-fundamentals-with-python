6dc739151ec1a8f6518b640a433cdb291dedf679
zlatinasadovska <134066942+zlatinasadovska@users.noreply.github.com>
2023-05-21 21:40:25 +0300

    Update even_numbers.py

--- basic_syntax_conditional_statements_and_loops/even_numbers.py
+++ basic_syntax_conditional_statements_and_loops/even_numbers.py
@@ -6,7 +6,6 @@ for i in range(n):
     if number % 2 != 0:
         print(f'{number} is odd!')
         break
-
-else:
-    print('All numbers are even.')
+    else:
+        print('All numbers are even.')
     
